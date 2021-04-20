33b9f2de4b98fa21fc7ab401686bccf4edb8ab48
sunitee <suniteebajpai2@gmail.com>
2021-04-20 12:02:32 -0400

    setup class Book, constructor and static methods

--- src/book.js
+++ src/book.js
@@ -1,7 +1,27 @@
 class Book {
 
-    static all =[]
-    
+    static allBooks =[]
 
+    constructor(book){
+        this.id = book.id
+        this.image_url = book.image_url
+        this.likes = book.likes
+        this.remarks = book.remarks
+        Book.allBooks.push(this)
+    }
+
+    static generateBooks(){
+        debugger
+        apiService.fetchBooks()
+            .then(data =>
+                data.forEach(book =>{
+                    const newBook = new Book(book)
+                    console.log(newBook)
+                })                               
+            )
+    }
+
+
+    
     
 }
