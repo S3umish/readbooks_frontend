4407e350db24f8adeb82c1c00dc4a23f811eae0c
sunitee <suniteebajpai2@gmail.com>
2021-04-20 18:33:35 -0400

    working on creating book cards

--- src/book.js
+++ src/book.js
@@ -1,3 +1,5 @@
+
+
 class Book {
 
     static allBooks =[]
@@ -12,26 +14,67 @@ class Book {
         Book.allBooks.push(this)
     }
 
-
-
     renderBook(){
+
+        // const bookContainer = document.querySelector(".books-container")
+
         let bookDiv = document.getElementById("List-of-Books")
         
-        bookDiv.innerHTML +=
+        // bookDiv.innerHTML +=
+
+        bookDiv.id ="cardID"
+        bookDiv.className ="card";
+
+        let newh2 = document.createElement("h2")
+        newh2.innerText = this.title;
+
+        const newh3 = document.createElement("h2")
+        newh3.innerText = this.likes;
+
+        const newh4 = document.createElement("h4")
+        newh4.innerText = this.remarks;
+
+        const newImg = document.createElement("img")
+        newImg.className ="book-avatar"
+        newImg.src = this.image_url;
+
+        const newButton = document.createElement("button")
+        newButton.setAttribute('data-id', this.id)
+        newButton.classname = "show-btn"
+        newButton.id ="showButton"
+        newButton.innerText = "Display";
+
+        const delButton = document.createElement("button")
+        delButton.setAttribute('data-id', this.id)
+        delButton.classname = "del-btn"
+        delButton.id ="delButton"
+        delButton.innerText = "Remove";
+
+        divCard.append(newH2, newImg, newH3, newH4, newB, delB);
+
+
         
-        "<img src=\""+this.image_url+"\">"+
-      
-        `<ul>
-        <h2>Book Title: <strong class="title">${this.title}</strong></h2>
-            <li>
-            Likes: <span class="likes">"${this.likes}."</span>
-            </li>
-            <li>
-            Remarks: <span class="remarks">${this.remarks}</span>
-            </li>
     
-        <button class="delete-button" onclick="deleteBook(${this.id})" data-id=${this.id}>Delete Book</button>
-        </ul>`
+
+        // let bookDiv = document.getElementById("List-of-Books")
+        
+        // bookDiv.innerHTML +=
+        
+        // "<img src=\""+this.image_url+"\">"+
+      
+        // `<ul>
+        // <h2>Book Title: <strong class="title">${this.title}</strong></h2>
+        //     <li>
+        //     Likes: <span class="likes">"${this.likes}."</span>
+        //     </li>
+        //     <li>
+        //     Remarks: <span class="remarks">${this.remarks}</span>
+        //     </li>
+    
+        // <button class="delete-button" onclick="deleteBook(${this.id})" data-id=${this.id}>Delete Book</button>
+        // </ul>`
+
+        
     }
 
 
